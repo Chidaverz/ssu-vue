3c8ecdb3860ba610eaed3ce59a3ab4d22a658f98
Prima Putra <dani.chidaverz@gmail.com>
2018-09-06 17:11:19 +0700

    Added controller and model for cafe

---
+++ app/Http/Controllers/API/CafesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cafe;
+use Request;
+
+class CafesController extends Controller
+{
+    public function getCafes()
+    {
+        $cafes = Cafe::all();
+
+        return response()->json($cafes);
+    }
+
+    public function getCafe($id)
+    {
+        $cafe = Cafe::where('id', '=', $id)->first();
+
+        return response()->json($cafes);
+    }
+
+    public function postNewCafe()
+    {
+        $cafe = new Cafe();
+
+        $cafe->name    = Request::get('name');
+        $cafe->address = Request::get('address');
+        $cafe->city    = Request::get('city');
+        $cafe->state   = Request::get('state');
+        $cafe->zip     = Request::get('zip');
+
+        $cafe->save();
+
+        return response()->json($cafe, 201);
+    }
+}
