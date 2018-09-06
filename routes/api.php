9c2dced08fea39f679846f3508f0487385e3f819
Prima Putra <dani.chidaverz@gmail.com>
2018-09-06 17:10:32 +0700

    Added API routes for cafe

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
-	Route::get('/user', function( Request $request ){
-		return $request->user();
-	});
+	Route::get('/cafes', 'API\CafesController@getCafes');
+	Route::post('/cafes', 'API\CafesController@postNewCafe');
+	Route::get('/cafe/{id}', 'API\CafesController@getCafe');
 });
