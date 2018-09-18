ec3e691b510f80f68c2ecd995c1ea2170970a517
Prima Putra <dani.chidaverz@gmail.com>
2018-09-18 08:05:09 +0700

    Use vuex module inside component

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,10 +1,27 @@
 <template>
-    
+    <div id="home">
+        <span v-show="cafesLoadStatus == 1">Loading</span>
+        <span v-show="cafesLoadStatus == 2">Cafes loaded successfully</span>
+        <span v-show="cafesLoadStatus == 3">Cafes loaded unsuccessfully</span>
+        <ul>
+            <li v-for="cafe in cafes" :key="cafe.id">{{ cafe.name }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-
+    created() {
+        this.$store.dispatch('loadCafes');
+    },
+    computed: {
+        cafesLoadStatus() {
+            return this.$store.getters.getCafeLoadStatus;
+        },
+        cafes() {
+            return this.$store.getters.getCafes;
+        }
+    }
 }
 </script>
 
